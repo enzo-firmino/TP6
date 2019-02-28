6fae44bc1de85658898528108c4f52b49e31f856
Enzo Firmino <enzo.firmino@etu.umontpellier.fr>
2019-02-28 11:00:45 +0100

    jeudi 28 fevrier 2018

--- src/main/java/fr/umontpellier/iut/App.java
+++ src/main/java/fr/umontpellier/iut/App.java
@@ -2,6 +2,21 @@ package fr.umontpellier.iut;
 
 public class App {
     public static void main(String[] args) {
-        throw new RuntimeException("Not yet implemented");
+
+        int[][] p1 = {{1, 2, 3}, {4, 0, 5}, {6, 7, 8}};
+        int[][] p2 = {{1, 0, 3}, {4, 2, 5}, {6, 7, 8}};
+        int[][] p3 = {{7, 5, 3}, {4, 2, 8}, {6, 1, 0}};
+
+
+        Taquin t1 = new Taquin(p1);
+        Taquin t2 = new Taquin(p1);
+        Taquin t3 = new Taquin(p2);
+
+
+        System.out.println(t1.equals(t3));
+
+
+
+        System.out.println(t3.genererFils());
     }
 }
