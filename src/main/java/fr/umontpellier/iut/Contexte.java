93a77d9a204cb3c03ab29ccb9f2a227af185c647
Enzo Firmino <enzo.firmino@etu.umontpellier.fr>
2019-03-12 17:55:58 +0100

    exo 3 12 mars

--- src/main/java/fr/umontpellier/iut/Contexte.java
+++ src/main/java/fr/umontpellier/iut/Contexte.java
@@ -1,6 +1,11 @@
 package fr.umontpellier.iut;
 
+import java.util.ArrayList;
+
 public class Contexte {
+
+    private Taquin taquin;
+    private ArrayList<Taquin>solution;
 
     public void resoudre() {
         throw new RuntimeException("Méthode non-implémentée");
